7582206b550927e5223c3502d53fd05ca80bf7c1
Nizar <nizar4790k@gmail.com>
2020-06-06 17:53:10 -0400

    first commit

---
+++ app/src/main/java/com/example/androidwebbrowser/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidwebbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
